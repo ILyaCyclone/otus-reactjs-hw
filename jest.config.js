19892d07750f3fe68b15ee1d39bb3a3263f2a01a
ILya Cyclone <ilya.cyclone@gmail.com>
2021-03-27 02:11:13 +0300

    basic project template

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    clearMocks: true,
+    coverageDirectory: "coverage",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.(js|ts)x?$": "<rootDir>/node_modules/babel-jest"
+    }
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface Props {
+}
+
+const App: React.FC<Props> = (props: Props) => {
+    return (
+        <h1>hi</h1>
+    );
+};
+
+export default App;
