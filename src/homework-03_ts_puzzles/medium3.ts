88ade600a27e7bd101b008f71ce8e349e9f2d19d
ILya Cyclone <ilya.cyclone@gmail.com>
2021-04-07 16:39:52 +0300

    homework 3 (ts puzzles) task

---
+++ src/homework-03_ts_puzzles/medium3.ts
@@ -0,0 +1,19 @@
+// Hint: type guards
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type FIXME = any;
+
+const orderStates = [
+  "initial",
+  "inWork",
+  "buyingSupplies",
+  "producing",
+  "fullfilled"
+] as const;
+
+type OrderState = typeof orderStates[number];
+
+export const getUserOrderStates = (orderStates: OrderState[]): FIXME =>
+  orderStates.filter(
+    (state) => state !== "buyingSupplies" && state !== "producing"
+  );
